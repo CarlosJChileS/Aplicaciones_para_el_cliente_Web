5b1aa42f2a77bf7580ec2fa466a3b968b9f7c9a9
CarlosJChileS <carloschilesilva@gmail.com>
2024-09-12 08:52:49 -0500

    5

--- Actividades/Ejercicio_5/ejercicio5.js
+++ Actividades/Ejercicio_5/ejercicio5.js
@@ -3,4 +3,13 @@
     var segundo_saludo = primer_saludo;
     primer_saludo = "hello";
     alert(segundo);
-</script>
+</script>;
+
+<script type="text2/javascript">
+    var dividendo = prompt("introduce el dividendo:")
+    var divisor =prompt("introduce el divisor:")
+    var resultado;
+    divisor !=0 ? resultado = dividendo/divisor:
+    alert("No es posible la division por cero");
+    alert("el resultado es:" + resultado);
+</script>;
